d704eb108c926ffd6c9a552194d4f1c40194e6f6
gjlam95 <gj_lam@hotmail.com>
2016-07-04 15:44:56 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,10 +3,10 @@ function processForm() {
     var ans = document.ansForm.temp.value;
     var tempType;
 
-    for (var i=0; i < document.tempForm.choice.length; i++) {
+    for (var i=0; i < document.ansForm.choice.length; i++) {
         
-        if (document.tempForm.choice[i].checked) {
-            tempType = document.tempForm.choice[i].value;
+        if (document.ansForm.choice[i].checked) {
+            tempType = document.ansForm.choice[i].value;
         }
     }
 
