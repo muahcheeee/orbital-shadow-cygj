4446209c661cc946c55e8328dcaaddfd12dcd412
gjlam95 <gj_lam@hotmail.com>
2016-07-08 11:16:01 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -9,7 +9,7 @@ function processPageOne() {
     }
 
     if (tempType == 'hall') {
-        var next = confirm("Correct!");
+        var next = confirm("Correct!\n\n" + "Professor Plum wrapped the gift in the Billard Room. Then then walked to the Conservatory, the Lounge, the Library, the Hall, the Ball Room, and the Dining Room. Plum remembered still having the gift with him in the Library. So we can rule out the rooms he was in before the Library: the Billard Room, the Conservatory, and the Lounge. Mrs White and Colonel Mustard checked the Ball Room and Library. And Professor Plum came into the Dining Room empty-handed. That leaves only the Hall.");
         if (next == true) {
         	window.history.back();
         }
@@ -68,7 +68,7 @@ function processPageTwo() {
         }
         
         else if (i === 4 && tempType[i] === ans[i]) {
-        	var next = confirm("Correct!");
+        	var next = confirm("Correct!\n\n" + "Professor Plum - Knife\n" + "Mr Green - Lead Pipe\n" + "Colonel Mustard - Candlestick\n" + "Miss Scarlet - Revolver\n" + "Mrs White - Wrench\n");
         	if (next == true) {
         		window.history.back();
             }
@@ -87,7 +87,7 @@ function processPageThree() {
     }
 
     if (tempType == 'white') {
-      var next = confirm("Correct!");
+      var next = confirm("Correct!\n\n" + "Gags:\n" + "Mr Green helping Mrs White put her head on the tray. Green 1, White 1\n" + "Professor Plum putting the Knife in his head. Plum 1\n" + "Colonel Mustard and Miss Scarlet faking her murder. Mustard 1, Scarlett 1\n" + "Mr Green and Colonel Mustard faking a duel to the death, with Mrs Peacock's help. Green 2, Mustard 2, Peacock 1\n" + "Professor Plum and Miss Scarlet staging a fake shooting. Plum 2 Scarlett 2\n" + "Only Mrs Peacock and Mrs White performed one gag before the final incident occured.\n");
       if (next == true) {
         window.history.back();
         }
